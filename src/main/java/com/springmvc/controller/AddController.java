6063928c4aaa3be544b49c044e13a48044e01ef1
deshpande-pavan <pavandeshpande60@gmail.com>
2019-08-09 00:24:42 +0530

    using @RequestParam

--- src/main/java/com/springmvc/controller/AddController.java
+++ src/main/java/com/springmvc/controller/AddController.java
@@ -1,10 +1,8 @@
 package com.springmvc.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springmvc.service.AddService;
@@ -12,12 +10,9 @@ import com.springmvc.service.AddService;
 @Controller
 public class AddController {
 
-
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
-		AddService add=new AddService();
-		int i = Integer.parseInt(request.getParameter("t1"));
-		int j = Integer.parseInt(request.getParameter("t2"));
+	public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j) {
+		AddService add = new AddService();
 		int k = add.add(i, j);
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("display");
